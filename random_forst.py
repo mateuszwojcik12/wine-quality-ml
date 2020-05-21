82d8ca0663a2f386a124ff38207edd62e07fd66f
Mateusz Wójcik <mateusz-wojcik@outlook.com>
2020-05-21 19:43:32 +0200

    Dodanie głównego jupyter notebooka

--- random_forst.py
+++ random_forst.py
@@ -4,6 +4,6 @@ red_wines = pd.read_csv("data.csv")
     X = red_wines.iloc[:, :-1]
     Y = red_wines.iloc[:, -1:]
 
-    X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X, Y, test_size=0.25)
-
-rf.fit(X_TRAIN, Y_TRAIN)
+#     X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X, Y, test_size=0.25)
+#
+# rf.fit(X_TRAIN, Y_TRAIN)
